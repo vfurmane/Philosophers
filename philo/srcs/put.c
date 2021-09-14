fe8dceb0371d4b66f5c7b5b33d648827626e985f
vfurmane <vfurmane@student.42.fr>
2021-09-14 13:05:20 +0200

    feat: display parameters to usage message
    Parameters corresponding to philosophers' behavior are now printed in
    the usage message.

--- philo/srcs/put.c
+++ philo/srcs/put.c
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 22:56:51 by vfurmane          #+#    #+#             */
-/*   Updated: 2021/09/03 23:02:12 by vfurmane         ###   ########.fr       */
+/*   Updated: 2021/09/04 09:11:04 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,11 @@
 
 void	print_usage(char *program_name)
 {
-	printf("Usage: %s [-h]\n\n", program_name);
+	printf("Usage: %s [-h] ", program_name);
+	printf("No_of_philos ");
+	printf("time_to_die ");
+	printf("time_to_eat ");
+	printf("time_to_sleep ");
+	printf("[No_of_times_each_philo_must_eat]\n\n");
 	printf("  -h, --help\tdisplay this help and exit\n");
 }
